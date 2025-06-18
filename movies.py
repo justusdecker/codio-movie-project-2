7a3af592c748144991a5e844ce21e411b54857c7
Justus <justus.d2025@gmail.com>
2025-06-18 15:47:08 +0200

    v

--- movies.py
+++ movies.py
@@ -10,33 +10,48 @@ from os.path import isfile
 from json import load, dumps, JSONDecodeError
 
 class Movie:
+    """
+    Represents a movie with its title, rating, and release year.
+
+    This class acts as a data model for a movie, providing properties to
+    access and modify its attributes. It stores the actual movie data
+    internally within a dictionary.
+    """
     def __init__(self,movie_data: dict):
         self.movie_data = movie_data
 
-
     def asdict(self) -> dict:
         """
-        Used to default some values e.g. on save
+        Returns a dictionary representation of the movie's core attributes.
+
+        This method is useful for serializing the movie object, e.g., for saving
+        to a file or sending over a network, ensuring a consistent structure.
+        It explicitly returns the 'title', 'rating', and 'release_year'
+        properties.
         """
         return {
             'title': self.title,
             'rating': self.rating,
             'release_year': self.release_year
         }
+    
     @property
     def title(self) -> str:
+        """ Gets the title of the movie. Defaults to 'n.a.' if not set. """
         return self.movie_data.get('title','n.a.')
     @title.setter
     def title(self, value: str):
         self.movie_data['title'] = value
     @property
     def rating(self) -> int:
+        """ Gets the rating of the movie. Defaults to 0 if not set. """
         return self.movie_data.get('rating',0)
     @rating.setter
     def rating(self, value: int):
         self.movie_data['rating'] = value
     @property
     def release_year(self) -> int:
+        """ Gets the release year of the movie. Defaults to 0 if not set. """
         return self.movie_data.get('release_year',0)
     @release_year.setter
     def release_year(self, value: int):
@@ -53,10 +68,17 @@ class MovieRank:
         return [i.title for i in self.movies]
 
     def save_movies(self):
+        """
+        Gets all your movies as an argument and saves them to the JSON file.
+        """
         with open(f'movies.json', 'w') as file_write:
             file_write.write(dumps([movie.asdict() for movie in self.movies],indent=4))
     
     def load_movies(self):
+        """
+        loads a list of dictionaries that
+        contains the movies information in the database into `self.movies`
+        """
         self.movies: list[Movie] = []
         file_name = f'movies.json'
         if isfile(file_name):
@@ -81,10 +103,15 @@ class MovieRank:
             print(f"{movie.title:<45} {movie.rating:<7} {movie.release_year}")
             
     def add_movie(self):
-
+        """
+        Adds a movie to the movies database and saves the new data to `movies.json`.
+        """
         title =  get_user_input_colorized("Movie title: ")
         rating = convert_to_float(get_user_input_colorized("Movie rating: "))
         release_year = convert_to_float(get_user_input_colorized("Movie release year: "))
+        
+        if not title:
+            error(MSG_NO_TITLE)
         
         if release_year < 1891:
             error(MSG_KINETOSSCOPE_NOT_INVENTED_YET)
@@ -106,25 +133,38 @@ class MovieRank:
                 }
             )
             self.movies.append(temp_movie)
-            
+        self.save_movies() 
+    
     def remove_movie(self):
-
+        """
+        Deletes a movie from the movies database and saves the new data to `movies.json`.
+        """
         title =  get_user_input_colorized("Movie title: ")
+        
+        if not title:
+            error(MSG_NO_TITLE)
+            
         if title in self.titles:
             
             self.movies.pop(self.titles.index(title))
         else:
             error(MSG_MOVIE_DOESNT_EXIST)
-            
+        self.save_movies()  
+         
     def edit_movie(self):
-
+        """
+        Updates a movie from the movies database and saves the new data to `movies.json`.
+        """
         title =  get_user_input_colorized("Movie title: ")
         
-        if title not in self.movies:
+        if not title:
+            error(MSG_NO_TITLE)
+        
+        if title not in self.titles:
             error(MSG_MOVIE_DOESNT_EXIST)
             return
         
-        rating = get_user_input_colorized("Movie rating: ")
+        rating = convert_to_float(get_user_input_colorized("Movie rating: "))
         
         if not rating: 
             error(MSG_RATING_IS_NOT_NUMERIC)
@@ -136,16 +176,11 @@ class MovieRank:
             self.movies[self.titles.index(title)].rating = rating
         else:
             error(MSG_MOVIE_DOESNT_EXIST)
-            
+        self.save_movies() 
+        
     def print_stats(self):
-        """
-        Calculates and prints various statistics about the movie ratings.
-
-        This method computes the average, median, worst, and best ratings 
-        from the 'movies' attribute of the instance. It then prints these statistics
-        in a user-friendly format.
-        """
-        ratings = [self.movies[i] for i in self.movies]
+        """ print movie stats like title, rating & release_year in a formatted way """
+        ratings = [i.rating for i in self.movies]
         median = ratings.copy()
         median.sort()
         median_hlen = len(median)//2
@@ -158,78 +193,64 @@ class MovieRank:
             median = round((median[median_hlen] +_median_1) / 2,2)
             print(median)
         average = sum(ratings) / len(ratings)
-        worst, ratingW = [],11
-        best, ratingB = [],-1
+        worst, rating_w = [],11
+        best, rating_b = [],-1
         
-        for key in self.movies:
-            if self.movies[key] > ratingB:
-                best.append(key)
-                ratingB = self.movies[key]
-            if self.movies[key] < ratingW:
-                worst.append(key)
-                ratingW = self.movies[key]
+        for movie in self.movies:
+            if movie.rating > rating_b:
+                best.append(movie.title)
+                rating_b = movie.rating
+            if movie.rating < rating_w:
+                worst.append(movie.title)
+                rating_w = movie.rating
         
         print(f"Average rating: {round(average,2)}. Median rating: {median}.\n{"-"*15}")
         print(f'Best Rating{"s" if len(best) > 1 else ""}\n{"-"*15}')
         for b in best:
-            print(f"Rating: {b} with {ratingB}/10")
+            print(f"Rating: {b} with {rating_b}/10")
         print(f'Worst Rating{"s" if len(worst) > 1 else ""}\n{"-"*15}')
         for w in worst:
-            print(f"Rating: {w} with {ratingW}/10. ")
+            print(f"Rating: {w} with {rating_w}/10. ")
         
     def print_random_movie(self):
-        """ 
-        Selects and prints a random movie and its rating from the instance's collection.
-
-        This method randomly picks one movie from the 'movies' attribute and
-        displays its title along with its corresponding rating to the console.
-        """
-        rndMovie = [i for i in self.movies][randint(0,len(self.movies)-1)]
-        print(f"{rndMovie}: {self.movies[rndMovie]}")
+        """ get a random movie and print it in a formatted way"""
+        rnd_movie = [i for i in self.movies][randint(0,len(self.movies)-1)]
+        print(f"{rnd_movie.title}: {rnd_movie.rating} released in: {rnd_movie.release_year}")
     
     def print_movies_by_rank(self):
-        """
-        Prints all movies stored in the instance, sorted by their rating in descending order.
-
-        Each movie's name and its rating (out of 10) are printed to the console.
-        The movie name is left-aligned and padded to a width of 35 characters.
-        """
-        listed = [[i,self.movies[i]] for i in self.movies]
+        """ get all movies by rank and print it in a formatted way"""
+        listed = [[i.rating,i.title] for i in self.movies]
         listed = sorted(listed,key=lambda x: x[1],reverse=True)
         for n, r in listed:
-            print(f"{n:<35} {r}/10")
+            print(f"{r:<35} {n}/10")
     def print_search(self):
-        """
-        Prompts the user for a search query and prints matching movie titles.
-
-        This method first asks the user for a search string using `get_user_input_colorized`.
-        It then attempts to find an exact, case-insensitive match within the movie titles.
-        If an exact match is found, it's printed.
-
-        If no exact match is found, the method iterates through all movie titles
-        and prints any movie whose title has a significant similarity to the search query,
-        as determined by the `compare_two_strings` function.
+        """ 
+        a searching method to get movies
         """
         value = get_user_input_colorized("Search: ").lower()
-        normal_movies = list(self.movies)
-        low_movies = [m.lower() for m in normal_movies]
+        low_movies = [m.title.lower() for m in self.movies]
         results = 0
         if value in low_movies:
             #normal_movies[low_movies.index(value)]
             results += 1
-            print(normal_movies[low_movies.index(value)])
+            movie = self.movies[low_movies.index(value)]
+            
+            print(f"{movie.title:<45} {movie.rating:<7} {movie.release_year}")
         if not results:
             for movie in self.movies:
-                if compare_two_strings(value, movie):
-                    print(movie)
+                if compare_two_strings(value, movie.title):
+                    print(f"{movie.title:<45} {movie.rating:<7} {movie.release_year}")
+    
     def plot_movies(self):
         """ Generates and displays a histogram of movie ratings. """
-        plt.hist([self.movies[i] for i in self.movies])
+        plt.hist([i.rating for i in self.movies])
         plt.show()
+    
     def byebye(self):
         """ The last thing the app will do before close """
-        self.save_movies()
+        
         print('bye!')
+    
     def update(self,inp):
         """
         Acts as a dispatcher for various movie management operations based on user input.
