43d9e585da7f3869b206ea6cd9fa81d5f761a701
Justus <justus.d2025@gmail.com>
2025-06-18 14:22:08 +0200

    Create constants.py

---
+++ bin/constants.py
@@ -0,0 +1,5 @@
+MSG_MOVIE_DOESNT_EXIST = "This movie doesn't exist!"
+MSG_RATING_IS_NOT_NUMERIC = "Rating is not a number!"
+MSG_INVALID_INPUT = "Invalid Input!"
+MSG_NO_RESULTS = 'No Results!'
+MSG_WRONG_RATING = "Enter a rating in range: 0 - 10"
